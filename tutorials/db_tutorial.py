19c43b6143a31befcaf8f87458c485862fa9fdd3
AI_Comedian <nickverastegui@gmail.com>
2025-11-05 11:10:22 -0500

    db tutorial addition

---
+++ tutorials/db_tutorial.py
@@ -0,0 +1,214 @@
+# Tutorial from: https://docs.langchain.com/oss/python/langgraph/agentic-rag#6-generate-an-answer
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# 1. preprocess documents
+from langchain_community.document_loaders import WebBaseLoader
+
+urls = [
+    "https://lilianweng.github.io/posts/2024-11-28-reward-hacking/",
+    "https://lilianweng.github.io/posts/2024-07-07-hallucination/",
+    "https://lilianweng.github.io/posts/2024-04-12-diffusion-video/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+
+docs[0][0].page_content.strip()[:1000]
+
+#Split the documents into smaller chunks for indexing
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=100, chunk_overlap=50
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+doc_splits[0].page_content.strip()
+
+# 2. Create a receiver tool
+
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import OpenAIEmbeddings
+
+vectorstore = InMemoryVectorStore.from_documents(
+    documents=doc_splits, embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+#Create a receiver took using LangChain's prebuilt
+from langchain_classic.tools.retriever import create_retriever_tool
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts.",
+)
+# Test
+response = retriever_tool.invoke({"query": "types of reward hacking"})
+
+print("retriever tool response" + str(response))
+
+# 3. Generate query
+# This is where it gets complicated. We start building nodes and edges for RAG graph
+#The components will operate on a MessagesState graph that contains a messages key with a list of chat messges
+
+from langgraph.graph import MessagesState
+from langchain.chat_models import init_chat_model
+
+response_model = init_chat_model("openai:gpt-4o", temperature=0)
+
+#So after initiating the chat model they use the bind tools method to attach the retriever tool
+def generate_query_or_respond(state: MessagesState):
+    """Call the model to generate a response based on the current state. Given
+    the question, it will decide to retrieve using the retriever tool, or simply respond to the user.
+    """
+    response = (
+        response_model
+        .bind_tools([retriever_tool]).invoke(state["messages"])
+    )
+    return {"messages": [response]}
+
+#Sample usage, this would return the AI introducing the user
+#input = {"messages": [{"role": "user", "content": "hello!"}]}
+#generate_query_or_respond(input)["messages"][-1].pretty_print()
+# ^ You can replace the input with something that would require semantic search
+
+# 4. Grade documents
+#Add a conditional edge grade_documents to determine whether the retrieved documents are relevant to the question
+
+from pydantic import BaseModel, Field
+from typing import Literal
+
+GRADE_PROMPT = (
+    "You are a grader assessing relevance of a retrieved document to a user question. \n "
+    "Here is the retrieved document: \n\n {context} \n\n"
+    "Here is the user question: {question} \n"
+    "If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n"
+    "Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."
+)
+
+#This class will be used in the definition to follow and defines the response for langchain
+
+class GradeDocuments(BaseModel):
+    """Grade documents using a binary score for relevance check."""
+
+    binary_score: str = Field(
+        description="Relevance score: 'yes' if relevant, or 'no' if not relevant"
+    )
+
+
+grader_model = init_chat_model("openai:gpt-4o", temperature=0)
+
+
+def grade_documents(
+    state: MessagesState,
+) -> Literal["generate_answer", "rewrite_question"]:
+    """Determine whether the retrieved documents are relevant to the question."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+
+    prompt = GRADE_PROMPT.format(question=question, context=context)
+    response = (
+        grader_model
+        .with_structured_output(GradeDocuments).invoke(
+            [{"role": "user", "content": prompt}]
+        )
+    )
+    score = response.binary_score
+
+    if score == "yes":
+        return "generate_answer"
+    else:
+        return "rewrite_question"
+
+
+
+# 5. rewrite question
+#Now we build the rewrite_question node. Because the retrieve tool can return irrelevant documents we do this
+REWRITE_PROMPT = (
+    "Look at the input and try to reason about the underlying semantic intent / meaning.\n"
+    "Here is the initial question:"
+    "\n ------- \n"
+    "{question}"
+    "\n ------- \n"
+    "Formulate an improved question:"
+)
+
+
+def rewrite_question(state: MessagesState):
+    """Rewrite the original user question."""
+    messages = state["messages"]
+    question = messages[0].content
+    prompt = REWRITE_PROMPT.format(question=question)
+    response = response_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [{"role": "user", "content": response.content}]}
+
+
+
+
+# 6. Generate an answer
+
+GENERATE_PROMPT = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer the question. "
+    "If you don't know the answer, just say that you don't know. "
+    "Use three sentences maximum and keep the answer concise.\n"
+    "Question: {question} \n"
+    "Context: {context}"
+)
+
+
+def generate_answer(state: MessagesState):
+    """Generate an answer."""
+    question = state["messages"][0].content
+    context = state["messages"][-1].content
+    prompt = GENERATE_PROMPT.format(question=question, context=context)
+    response = response_model.invoke([{"role": "user", "content": prompt}])
+    return {"messages": [response]}
+
+
+# 7. Assemble a graph
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+
+workflow = StateGraph(MessagesState)
+
+# Define the nodes we will cycle between
+workflow.add_node(generate_query_or_respond)
+workflow.add_node("retrieve", ToolNode([retriever_tool]))
+workflow.add_node(rewrite_question)
+workflow.add_node(generate_answer)
+
+workflow.add_edge(START, "generate_query_or_respond")
+
+# Decide whether to retrieve
+workflow.add_conditional_edges(
+    "generate_query_or_respond",
+    # Assess LLM decision (call `retriever_tool` tool or respond to the user)
+    tools_condition,
+    {
+        # Translate the condition outputs to nodes in our graph
+        "tools": "retrieve",
+        END: END,
+    },
+)
+
+# Edges taken after the `action` node is called.
+workflow.add_conditional_edges(
+    "retrieve",
+    # Assess agent decision
+    grade_documents,
+)
+workflow.add_edge("generate_answer", END)
+workflow.add_edge("rewrite_question", "generate_query_or_respond")
+
+# Compile
+graph = workflow.compile()
